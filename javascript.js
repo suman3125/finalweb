34e66b9e1aae4a3f5ea805142c737c85711d6043
suman3125 <53547265+suman3125@users.noreply.github.com>
2019-10-19 20:42:51 +1100

    making java code to make responsive

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
-function myFunction() {
-    var x = document.getElementById("mytopmenu");
-    if (x.className === "topmenu") {
-      x.className += " responsive";
-    } else {
-      x.className = "topmenu";
-    }
-  }
+(function($) { 
+  $(function() { 
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); 
+})(jQuery);
